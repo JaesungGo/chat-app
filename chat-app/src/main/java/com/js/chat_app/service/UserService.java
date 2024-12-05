e5e94a89229cec5d6d317539a98713607cb3680f
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-12-05 22:46:23 +0900

    Feat: 유저 회원 가입, 회원 찾기 기능 구현

--- chat-app/src/main/java/com/js/chat_app/service/UserService.java
+++ chat-app/src/main/java/com/js/chat_app/service/UserService.java
@@ -1,4 +1,34 @@
 package com.js.chat_app.service;
 
+
+import com.js.chat_app.domain.User;
+import com.js.chat_app.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // 회원 가입
+    public User createUser(User user){
+        // 이메일 중복 체크
+        if(userRepository.existsByEmail(user.getEmail())){
+            throw new RuntimeException("이메일이 이미 존재합니다.");
+        }
+
+        // 비밀번호 암호화
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+
+    }
+
+    // 회원 찾기
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
+    }
 }
