dbb11bf4dba18349c465d52b37fb56344ab48b05
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-12-05 22:45:25 +0900

    Feat: 채팅방 사용자 엔티티 -> 매니저,멤버 역할 구분

--- chat-app/src/main/java/com/js/chat_app/domain/chat/ChatRoomUser.java
+++ chat-app/src/main/java/com/js/chat_app/domain/chat/ChatRoomUser.java
@@ -1,5 +1,6 @@
 package com.js.chat_app.domain.chat;
 
+import com.js.chat_app.domain.User;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,14 +14,24 @@ public class ChatRoomUser {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long chatuserId;
 
+    @ManyToOne @JoinColumn(name = "user_id")
+    private User user;
+
     @ManyToOne @JoinColumn(name = "chat_room_id")
     private ChatRoom chatRoom;
 
     @Column(name = "joined_at")
     private LocalDateTime joinedAt;
 
+    private RoomRole roomRole;
+
     @PrePersist
     protected void onCreate(){
         joinedAt = LocalDateTime.now();
+    }
+
+    public enum RoomRole{
+        ROOM_MANAGER,
+        ROOM_MEMBER
     }
 }
