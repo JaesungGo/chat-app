c2e627bc96a4f13e0af096c8f3bcf8fb296cd990
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-12-08 02:11:17 +0900

    Fix: 쿠키 기반의 JWT인증 방식으로 변경, 로그아웃 기능 추가

--- chat-app/src/main/java/com/js/chat_app/service/jwt/AuthService.java
+++ chat-app/src/main/java/com/js/chat_app/service/jwt/AuthService.java
@@ -1,12 +1,15 @@
 package com.js.chat_app.service.jwt;
 
 import com.js.chat_app.domain.user.User;
-import com.js.chat_app.domain.user.UserDTO;
+import com.js.chat_app.domain.user.userDTO.LoginRequest;
 import com.js.chat_app.repository.user.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -16,29 +19,44 @@ public class AuthService {
     private final JwtProvider jwtProvider;
     private final PasswordEncoder passwordEncoder;
 
-    public UserDTO.TokenResponse login(UserDTO.LoginRequest request) {
+    // 로그인
+    public void login(LoginRequest request, HttpServletResponse httpServletResponse) {
 
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 이메일 입니다."));
+
+        user.setStatus(User.Status.ACTIVE);
+        user.setLastLoginAt(LocalDateTime.now());
+        userRepository.save(user);
 
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new RuntimeException("비밀번호가 일치하지 않습니다.");
         }
 
         String token = jwtProvider.createToken(user);
-
-        return UserDTO.TokenResponse.builder()
-                .accessToken(token)
-                .email(user.getEmail())
-                .username(user.getUserName())
-                .build();
-
+        jwtProvider.addCookie(token,httpServletResponse);
     }
 
-    public User getCurUser(String token){
+    // 현재 유저정보 찾기
+    public User getCurUser(HttpServletRequest httpServletRequest){
+        String token = jwtProvider.cookieToToken(httpServletRequest);
         String userEmail = jwtProvider.getEmail(token);
         return userRepository.findByEmail(userEmail)
                 .orElseThrow(()-> new RuntimeException("토큰의 이메일이 DB의 이메일과 다르거나 없음"));
     }
 
+    // 로그 아웃
+    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+        String token = jwtProvider.cookieToToken(httpServletRequest);
+        String email = jwtProvider.getEmail(token);
+
+        User user = userRepository.findByEmail(email)
+                        .orElseThrow(()-> new RuntimeException("존재하지 않는 이메일 입니다."));
+
+        user.setStatus(User.Status.INACTIVE);
+        user.setLastLogoutAt(LocalDateTime.now());
+        userRepository.save(user);
+
+        jwtProvider.removeCookie(httpServletResponse);
+    }
 }
