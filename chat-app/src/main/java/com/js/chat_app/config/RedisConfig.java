55e101fe98b1466f2f3f5cafeeb12e6376137123
JaesungGo <102339979+JaesungGo@users.noreply.github.com>
2024-12-11 03:08:09 +0900

    Feat: Redis 관련 설정 추가

--- chat-app/src/main/java/com/js/chat_app/config/RedisConfig.java
+++ chat-app/src/main/java/com/js/chat_app/config/RedisConfig.java
@@ -1,4 +1,49 @@
 package com.js.chat_app.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Lettuce를 사용한 설정
+ */
+
+@Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    /**
+     * Redis 연결 관리 팩토리 객체 생성 및 빈 등록 (RedisConnectionFactory)
+     * Lettuce(Redis 클라이언트 사용) -> Redis 서버에 대한 연결 생성 및 관리
+     * @return
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    /**
+     * RedisConnectionFactory를 Redis 연결에 사용하고
+     * key,value 값을 Redis에서 사용할 수 있도록 직렬화 한다.
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return redisTemplate;
+    }
 }
